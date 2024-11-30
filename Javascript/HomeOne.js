9b20bb2a4631d01ba3e93a72bef162be951dc08f
Bayu Kresna Mukti <kresnamukti386@gmail.com>
2024-11-30 18:53:43 +0700

    Repo Baru Buat Belajar Dari Rumah

---
+++ Javascript/HomeOne.js
@@ -0,0 +1,11 @@
+function bikinFunction(){
+    document.getElementById('Lamp').style.display = 'block';
+}
+
+function Home(){
+    document.getElementById('Lamp').style.display = 'none';
+}
+
+function ujiCoba(){
+    window.print();
+}
